aebd9f69d586057026850b8830cd9bcf8b405a7a
Hayden Bleasel <hello@haydenbleasel.com>
2022-10-01 13:27:47 -0400

    Migrate files from Daylight

---
+++ README.md
@@ -0,0 +1,52 @@
+# command-bar
+
+`command-bar` is a pre-configured [cmdk](https://github.com/pacocoursey/cmdk) instance with:
+
+- A useful `useCommandBar` hook
+- Predefined keyboard shortcuts
+- Gamepad support
+- Subtle pop animation + automatic input re-focusing when changing page
+
+## Usage
+
+You can use `command-bar` the same way you would use `cmdk`, with a few additions:
+
+```tsx
+import { CommandBar, useCommandBar } from '@haydenbleasel/command-bar';
+
+const App = () => {
+  const commandBar = useCommandBar();
+
+  return (
+    <CommandBar.Dialog>
+      <CommandBar.Container>
+        <CommandBar.Input />
+        <CommandBar.List>
+          <CommandBar.Empty>Empty State</CommandBar.Empty>
+          <CommandBar.Loading />
+          <CommandBar.Group>
+            <CommandBar.Item>Item</CommandBar.Item>
+            <CommandBar.Separator>
+            <CommandBar.Item>Item</CommandBar.Item>
+          </CommandBar.Group>
+        </CommandBar.List>
+      </CommandBar.Container>
+    </CommandBar.Dialog>
+  );
+};
+```
+
+## Keyboard Shortcuts
+
+- Meta Key + `k`: Toggles the command bar. On Mac, this is `cmd+k`. On Windows, this is `ctrl+k`.
+- `Backspace`: If the search input is empty, goes back to the root.
+- `Escape`: If you're on the root, closes the command bar. If you're not, goes back to the root.
+
+## Gamepad Support
+
+Relies on [@haydenbleasel/use-gamepad-events](https://github.com/haydenbleasel/use-gamepad-events) to map Gamepad buttons to commandbar actions:
+
+- `y`: opens and closes the commandbar, utilising `toggleOpen` from `useCommandBar`
+- `b`: goes back to the root, utilising `setPage('')` from `useCommandBar`
+- `a`: selects the current item, utilising `select` from `useCommandBar`
+- `up` / `down`: navigates the commandbar, utiling `setIndex` from `useCommandBar`
---
+++ src/index.tsx
@@ -0,0 +1,209 @@
+import { useKeyboardEvent } from '@react-hookz/web';
+import { Command } from 'cmdk';
+import type { FC, HTMLProps, RefObject } from 'react';
+import { createContext, useContext, useEffect, useRef } from 'react';
+import create from 'zustand';
+import useGamepadEvents from '@haydenbleasel/use-gamepad-events';
+
+export const useCommandBar = create<{
+  open: boolean;
+  page: string;
+  search: string;
+  value: string;
+  toggleOpen: (open?: boolean) => void;
+  setPage: (page: string) => void;
+  setSearch: (search: string) => void;
+  setValue: (value: string) => void;
+  index: number;
+  setIndex: (value: number | ((index: number) => number)) => void;
+  select: () => void;
+}>((set) => ({
+  open: false,
+  page: '',
+  search: '',
+  value: '',
+  toggleOpen: (newOpen?: boolean) =>
+    set((state) => ({
+      open: typeof newOpen === 'boolean' ? newOpen : !state.open,
+    })),
+  setPage: (page) => set({ page }),
+  setSearch: (search) => set({ search }),
+  setValue: (value) => set({ value }),
+  index: 0,
+  setIndex: (value) =>
+    set((state) => {
+      const newIndex = typeof value === 'function' ? value(state.index) : value;
+
+      const allElements = document.querySelectorAll('[cmdk-item]');
+      const element = allElements[newIndex] as HTMLElement | undefined;
+
+      if (newIndex < 0 || newIndex >= allElements.length) {
+        return { index: state.index };
+      }
+
+      if (element) {
+        allElements.forEach((el) => {
+          el.removeAttribute('aria-selected');
+        });
+        element.setAttribute('aria-selected', 'true');
+        element.scrollIntoView({
+          behavior: 'smooth',
+          block: 'nearest',
+          inline: 'nearest',
+        });
+      }
+
+      return { index: newIndex };
+    }),
+  select: () => {
+    const activeElement = document.querySelector(
+      '[cmdk-item][aria-selected="true"]'
+    );
+
+    if (activeElement) {
+      (activeElement as HTMLDivElement).click();
+    }
+  },
+}));
+
+const InputRefContext = createContext<RefObject<HTMLInputElement> | null>(null);
+
+const Input: FC<typeof Command.Input> = (props) => {
+  const { search, setSearch, page } = useCommandBar();
+  const inputRef = useContext(InputRefContext);
+
+  return (
+    <Command.Input
+      placeholder={page ? `${page}...` : 'Start typing...'}
+      value={search}
+      onValueChange={setSearch}
+      ref={inputRef}
+      {...props}
+    />
+  );
+};
+
+const Container: FC<HTMLProps<HTMLDivElement>> = (props) => {
+  const dialogRef = useRef<HTMLDivElement>(null);
+  const { setSearch, page } = useCommandBar();
+  const inputRef = useContext(InputRefContext);
+
+  useEffect(() => {
+    inputRef?.current?.focus();
+    setSearch('');
+
+    dialogRef.current?.style.setProperty('transform', 'scale(0.98)');
+
+    setTimeout(() => {
+      dialogRef.current?.style.setProperty('transform', 'scale(1)');
+    }, 100);
+  }, [inputRef, page, setSearch]);
+
+  return <div ref={dialogRef} {...props} />;
+};
+
+const Dialog: FC<typeof Command.Dialog> = (props) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const gamepadEvents = useGamepadEvents();
+  const {
+    open,
+    toggleOpen,
+    value,
+    setValue,
+    search,
+    page,
+    setPage,
+    setIndex,
+    select,
+  } = useCommandBar();
+
+  useKeyboardEvent(
+    true,
+    (event) => {
+      if (event.key === 'k' && event.metaKey) {
+        toggleOpen();
+        return;
+      }
+
+      if (
+        (!search || page) &&
+        (event.key === 'Escape' || event.key === 'Backspace')
+      ) {
+        setPage('');
+        return;
+      }
+
+      if (event.key === 'Escape') {
+        toggleOpen(false);
+      }
+    },
+    [],
+    { eventOptions: { passive: true } }
+  );
+
+  gamepadEvents.on('y', toggleOpen);
+
+  gamepadEvents.on('down', () => {
+    if (typeof window === 'undefined' || !open) {
+      return;
+    }
+
+    setIndex((index) => index + 1);
+  });
+
+  gamepadEvents.on('a', () => {
+    if (typeof window === 'undefined' || !open) {
+      return;
+    }
+
+    select();
+  });
+
+  gamepadEvents.on('b', () => {
+    if (typeof window === 'undefined' || !open) {
+      return;
+    }
+
+    setPage('');
+  });
+
+  gamepadEvents.on('up', () => {
+    if (typeof window === 'undefined' || !open) {
+      return;
+    }
+
+    setIndex((index) => index - 1);
+  });
+
+  return (
+    <InputRefContext.Provider value={inputRef}>
+      <Command.Dialog
+        value={value}
+        onValueChange={setValue}
+        open={open}
+        onOpenChange={toggleOpen}
+        label="Global Command Menu"
+        {...props}
+      />
+    </InputRefContext.Provider>
+  );
+};
+
+const CommandBar = {
+  Dialog,
+  Container,
+  Input,
+  List: Command.List,
+  Empty: Command.Empty,
+  Group: Command.Group,
+  Item: Command.Item,
+  Loading: Command.Loading,
+  Separator: Command.Separator,
+};
+
+const exports = {
+  useCommandBar,
+  CommandBar,
+};
+
+export default exports;
